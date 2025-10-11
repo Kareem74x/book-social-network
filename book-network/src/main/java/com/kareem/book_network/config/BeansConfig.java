1cdeb6e29f3371d4bffe0646351fe49768587617
Kareem74x <hpg350@gmail.com>
2025-10-11 05:50:39 +0300

    feat: add application auditor aware

--- book-network/src/main/java/com/kareem/book_network/config/BeansConfig.java
+++ book-network/src/main/java/com/kareem/book_network/config/BeansConfig.java
@@ -3,6 +3,7 @@ package com.kareem.book_network.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -39,4 +40,9 @@ public class BeansConfig {
         return config.getAuthenticationManager();
     }
 
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
+    }
 }
